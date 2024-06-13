7cf447490c6dfbcdb9ad6b31d0895e10ff82efc6
twknab <20636750+twknab@users.noreply.github.com>
2024-06-13 14:53:42 -0700

    refactor chip classes

--- src/components/ProductSidebar.tsx
+++ src/components/ProductSidebar.tsx
@@ -12,15 +12,25 @@ function ProductSidebar({product}: ProductSidebarProps) {
   console.log(product)
   return (
     <div className="bg-white w-full sm:w-[522px] pt-6 shadow-lg">
-      <img src={product.image} width="163px" height="206px" alt="Shark Ninja" className="mx-auto" />
-      <h1 className="text-[1.39rem] text-center font-normal mt-3">{product.title}</h1>
+      <img
+        src={product.image}
+        width="163px"
+        height="206px"
+        alt="Shark Ninja"
+        className="mx-auto"
+      />
+      <h1 className="text-[1.39rem] text-center font-normal mt-3">
+        {product.title}
+      </h1>
       <p className="text-md text-center mt-1 mb-5 text-[#A7B7C9] pl-14 pr-14">
         {product.subtitle}
       </p>
       <div className="border-t border-b mb-6 pt-4 pb-4 pl-5 pr-5">
-        {product.tags.map(tag =>
-          <div className="chip">{tag}</div>
-        )}
+        {product.tags.map((tag) => (
+          <div className="px-6 py-0.5 border rounded-md inline-block mx-1 my-2 text-gray-600 border-gray-200">
+            {tag}
+          </div>
+        ))}
       </div>
     </div>
   );
