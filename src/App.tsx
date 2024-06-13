7ecf03dae953c3472f09f8253755c971c1bafb6e
twknab <20636750+twknab@users.noreply.github.com>
2024-06-13 12:44:18 -0700

    adjust sidebar height

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="h-screen bg-[#F6F8FA]">
       <Navigation />
-      <div className="flex flex-col sm:flex-row h-content mt-[79px] m-6 sm:ml-6 h-content">
+      <div className="flex flex-col sm:flex-row h-content mt-[79px] m-6 sm:ml-6 h-3/4">
         <ProductSidebar />
         <div className="w-full mt-[65px] sm:mt-0 ml-0 sm:ml-6">
           <SalesGraph />
--- src/components/ProductSidebar.tsx
+++ src/components/ProductSidebar.tsx
@@ -2,7 +2,7 @@ import sharkNinjaImage from "../assets/shark-ninja.png";
 
 function ProductSidebar() {
   return (
-    <div className="bg-white w-full sm:w-[512px] pt-6 shadow-lg h-100">
+    <div className="bg-white w-full sm:w-[512px] pt-6 shadow-lg">
       <img src={sharkNinjaImage} alt="Shark Ninja" className="mx-auto" />
       <h1 className="text-[1.39rem] text-center font-normal mt-3">Shark Ninja</h1>
       <p className="text-md text-center mt-1 mb-5 text-[#A7B7C9] pl-6 pr-6">
